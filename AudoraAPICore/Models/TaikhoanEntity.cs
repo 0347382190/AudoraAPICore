9f92ec8d30761ad6b696274096603c690c9e5671
Nguyễn Hoài <hoainhoc101@gmail.com>
2023-03-20 16:36:39 +0700

    commit entity Nhanvien, phim, phongchieu, loaive, loaiphim

--- AudoraAPICore/Models/TaikhoanEntity.cs
+++ AudoraAPICore/Models/TaikhoanEntity.cs
@@ -1,7 +1,20 @@
-﻿namespace AudoraAPICore.Models
+﻿/*
+ *Name:TaikhoanEntity.cs
+ *03/20/2023: Nguyễn Thị Thu Hoài
+ *DESC: Create
+ */
+namespace AudoraAPICore.Models
 {
     public class TaikhoanEntity
     {
-     
+        private int m_PK_iTaikhoanID;
+        private string m_sPhanquyen;
+        private string m_sMatkhau;
+        private string m_sEmail;
+
+        public int PK_iTaikhoanID { get => m_PK_iTaikhoanID; set => m_PK_iTaikhoanID = value; }
+        public string sPhanquyen { get => m_sPhanquyen; set => m_sPhanquyen = value; }
+        public string sMatkhau { get => m_sMatkhau; set => m_sMatkhau = value; }
+        public string sEmail { get => m_sEmail; set => m_sEmail = value; }
     }
 }
