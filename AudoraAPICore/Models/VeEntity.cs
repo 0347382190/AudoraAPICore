fe5fcd71570bee4c917992218a8acb6b527e98e7
Nguyễn Hoài <hoainhoc101@gmail.com>
2023-03-20 15:21:48 +0700

    commit entity Ve, Tai Khoan

---
+++ AudoraAPICore/Models/VeEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using System.Runtime.InteropServices;
+
+namespace AudoraAPICore.Models
+{
+    public class VeEntity
+    {
+        private int m_PK_iVeID;
+        private int m_FK_iHoadonID;
+        private int m_FK_iGheID;
+
+        public int PK_iVeID { get => m_PK_iVeID; set => m_PK_iVeID = value; }
+        public int FK_iHoadonID { get => m_FK_iHoadonID; set => m_FK_iHoadonID = value; }
+        public int FK_iGheID { get => m_FK_iGheID; set => m_FK_iGheID = value; }
+    }
+}
