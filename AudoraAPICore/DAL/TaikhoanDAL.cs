ac73b27b5a3cb9edaac4b27f20aafb1cb4be47a7
Nguyễn Hoài <hoainhoc101@gmail.com>
2023-03-23 15:35:16 +0700

    commit DAL: TaikhoanDAL 03232023

---
+++ AudoraAPICore/DAL/TaikhoanDAL.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace AudoraAPICore.DAL
+{
+    public class TaikhoanDAL
+    {
+        public IConfigurationRoot GetConfiguration()
+        {
+            var buider = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            return buider.Build();
+        }
+        public string GetconnectString()
+        {
+            var configuation = GetConfiguration();
+            string chuoiketnoi = configuation.GetSection("ConnectionStrings").GetSection("db_Audora").Value;
+            return chuoiketnoi;
+        }
+        public void LoginTaikhoan()
+        {
+            using (SqlConnection cnn = new SqlConnection(GetconnectString()))
+            {
+                cnn.Open();
+                using (SqlCommand cmd = new SqlCommand("",cnn))
+                {
+
+                }
+            }
+        }
+       
+    }
+}
