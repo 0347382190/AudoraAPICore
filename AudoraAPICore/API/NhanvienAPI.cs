cbbd810a32bb3b9a949ca08b750cb4d1d9e95f42
Hong Ngoc <ngocnguyen71e11@gmail.com>
2023-04-04 20:28:02 +0700

    commit NhavienAPI, KhachhangAPI, phongchieuAPI

--- AudoraAPICore/API/NhanvienAPI.cs
+++ AudoraAPICore/API/NhanvienAPI.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+//using System.Web.Http;
+using AudoraAPICore.DAL;
+using AudoraAPICore.Models;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,28 +19,38 @@ namespace AudoraAPICore.API
     {
         // GET: api/<NhanvienAPI>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public List<NhanvienEntity> GetAll()
         {
-            return new string[] { "value1", "value2" };
+            List<NhanvienEntity> lstNhanvien = new List<NhanvienEntity>();
+            NhanvienDAL nhanvienDAL = new NhanvienDAL();
+            lstNhanvien = nhanvienDAL.Danhsachnhanvien();
+            return lstNhanvien;
         }
 
         // GET api/<NhanvienAPI>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public List<NhanvienEntity> Get(string PK_sCCCD)
         {
-            return "value";
+            NhanvienDAL nhanvienDAL = new NhanvienDAL();
+            //return "";
+            return nhanvienDAL.Timkiemnhanvien(PK_sCCCD);
         }
 
         // POST api/<NhanvienAPI>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] NhanvienEntity nhanvienEntity)
         {
+            NhanvienDAL nhanvienDAL = new NhanvienDAL();
+            nhanvienDAL.Themnhanvien(nhanvienEntity);
         }
+
 
         // PUT api/<NhanvienAPI>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] NhanvienEntity nhanvienEntity)
         {
+            NhanvienDAL nhanvienDAL = new NhanvienDAL();
+            nhanvienDAL.Themnhanvien(nhanvienEntity);
         }
 
         // DELETE api/<NhanvienAPI>/5
--- AudoraAPICore/API/PhongchieuAPI.cs
+++ AudoraAPICore/API/PhongchieuAPI.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+//using System.Web.Http;
+using AudoraAPICore.DAL;
+using AudoraAPICore.Models;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,34 +19,45 @@ namespace AudoraAPICore.API
     {
         // GET: api/<PhongchieuAPI>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public List<PhongchieuEntity> GetAll()
         {
-            return new string[] { "value1", "value2" };
+            List<PhongchieuEntity> lstPhongchieu = new List<PhongchieuEntity>();
+            PhongchieuDAL phongchieuDAL = new PhongchieuDAL();
+            lstPhongchieu = phongchieuDAL.Danhsachphongchieu();
+            return lstPhongchieu;
         }
 
         // GET api/<PhongchieuAPI>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public List<PhongchieuEntity> Get(long PK_iPhongchieuID)
         {
-            return "value";
+            PhongchieuDAL phongchieuDAL = new PhongchieuDAL();
+            //return "";
+            return phongchieuDAL.Timkiemphongchieu(PK_iPhongchieuID);
         }
 
         // POST api/<PhongchieuAPI>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] PhongchieuEntity phongchieuEntity)
         {
+            PhongchieuDAL phongchieuDAL = new PhongchieuDAL();
+            phongchieuDAL.Themphongchieu(phongchieuEntity);
         }
 
         // PUT api/<PhongchieuAPI>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] PhongchieuEntity phongchieuEntity)
         {
+            PhongchieuDAL phongchieuDAL = new PhongchieuDAL();
+            phongchieuDAL.Suaphongchieu(phongchieuEntity);
         }
 
         // DELETE api/<PhongchieuAPI>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public void Delete(long PK_iPhongchieuID)
         {
+            PhongchieuDAL phongchieuDAL = new PhongchieuDAL();
+            phongchieuDAL.Xoaphongchieu(PK_iPhongchieuID);
         }
     }
 }
