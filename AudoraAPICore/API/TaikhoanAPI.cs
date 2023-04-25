5b18fdd0be8c0c7feed0329a710489417fdce923
Nguyễn Hoài <hoainhoc101@gmail.com>
2023-04-25 15:44:26 +0700

    commtir

--- AudoraAPICore/API/TaikhoanAPI.cs
+++ AudoraAPICore/API/TaikhoanAPI.cs
@@ -1,6 +1,11 @@
 ﻿using AudoraAPICore.DAL;
 using AudoraAPICore.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,14 +15,38 @@ namespace AudoraAPICore.API
     [ApiController]
     public class TaikhoanAPI : ControllerBase
     {
+        TaikhoanDAL taikhoanDAL = new TaikhoanDAL();
         // GET: api/<TaikhoanAPI>
         [HttpGet("{sEmail}/{sMatkhau}")]
         //[RequireHttps]
-        public string login(string sEmail, string sMatkhau)
+        public string login()
         {
-            TaikhoanDAL taikhoanDAL = new TaikhoanDAL();
-            return taikhoanDAL.LoginTaikhoan(sEmail, sMatkhau);
+
+            // Lấy tiêu đề Authorization từ yêu cầu HTTP
+            string authHeader = Request.Headers["Authorization"];
+
+            // Nếu tiêu đề Authorization không tồn tại hoặc không bắt đầu bằng "Basic", trả về mã trạng thái 401 Unauthorized
+            if (string.IsNullOrEmpty(authHeader) || !authHeader.StartsWith("Basic "))
+            {
+                Response.Headers["WWW-Authenticate"] = "Basic";
+                // return (int)HttpStatusCode.Unauthorized;
+            }
+            // Giải mã chuỗi thông tin xác thực
+            string encodedUsernamePassword = authHeader.Substring("Basic ".Length).Trim();
+            byte[] decodedBytes = Convert.FromBase64String(encodedUsernamePassword);
+            string[] usernamePassword = Encoding.UTF8.GetString(decodedBytes).Split(':');
+            string username = usernamePassword[0];
+            string password = usernamePassword[1];
+
+            // Kiểm tra tên đăng nhập và mật khẩu trong cơ sở dữ liệu hoặc các phương tiện lưu trữ khác
+            bool isAuthenticated = taikhoanDAL.LoginTaikhoan(username, password)!=null?true:false;
+            if (isAuthenticated == true)
+            {
+                 return username;
+            }
+            return "sai rồi";
         }
     }
+   
 }
 
